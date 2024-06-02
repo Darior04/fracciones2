82f419c274bad6ca392f4844b151824ce93397e7
Dario Chavez <dc6175370@gmail.com>
2024-06-01 19:08:49 -0600

    niveles 2

---
+++ nivel2division.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fracciones
+{
+    public partial class nivel2division : Form
+    {
+        public nivel2division()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ nivel2multiplicacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fracciones
+{
+    public partial class nivel2multiplicacion : Form
+    {
+        public nivel2multiplicacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
